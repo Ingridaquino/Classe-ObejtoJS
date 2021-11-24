71b39af62001d76298927eea4dc50dc7330ceb72
Ingrid Aquino <ingridaquinosoares@live.com>
2021-11-24 15:58:20 -0300

    exercicio e projeto

---
+++ Projeto React/tema_05_agenda/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+
+
+function App() {  
+    return (  
+        <div className="container">  
+           <Header title="tarefas"/>       
+      </div>  
+    );  
+}  
+	  
+export default App;
