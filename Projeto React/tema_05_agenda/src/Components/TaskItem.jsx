9196a7ab91551e4aaa214a1857e7e293908d7042
Ingrid Aquino <ingridaquinosoares@live.com>
2021-11-24 21:59:22 -0300

    part 2

---
+++ Projeto React/tema_05_agenda/src/Components/TaskItem.jsx
@@ -0,0 +1,9 @@
+const TaskItem = () => {  
+    return (  
+        <div className="task">  
+            <h3>Minha Tarefa</h3>  
+        </div>  
+    );  
+};  
+ export default TaskItem;  
+    
